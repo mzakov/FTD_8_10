9e66d66b4603ea50a6535879b31517bfb086447f
mzakov <m.zakov@gmail.com>
2016-08-10 11:58:15 -0500

    File Project completed!

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,41 @@
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
 
 public class Main {
 	public static void main(String args[]) throws IOException {
-		
-		File f = new File("c:\temp\newFile.txt");
-		
-		InputStream in = new FileInputStream(f);
-		OutputStream out = new FileOutputStream("out");
-		
-		BufferedInputStream buff = new BufferedInputStream(in);
-		
-		BufferedReader read = new BufferedReader(new InputStreamReader(buff));
-		
-		System.out.println(read.readLine());
-		
-		
+
+		File file = new File("c:\\temp\\student.txt");
+
+		if (!file.exists()) {
+			Student studentObj = new Student("Tom", "FL", "Tampa");
+			BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+
+			writer.write(studentObj.toString());
+			writer.close();
+
+		} else {
+
+			BufferedReader reader = new BufferedReader(new FileReader(file));
+
+			Student studentObj2 = new Student(reader.readLine(), reader.readLine(), reader.readLine());
+			reader.close();
+
+			System.out.println(studentObj2);
+
+		}
+
+		// InputStream in = new FileInputStream(file);
+		// OutputStream out = new FileOutputStream("out");
+
+		// BufferedInputStream buff = new BufferedInputStream(in);
+
+		// BufferedReader read = new BufferedReader(new FileReader(file));
+
+		// System.out.println(read.readLine());
+
 	}
 }
---
+++ src/Student.java
@@ -0,0 +1,41 @@
+
+public class Student {
+
+	private String name;
+
+	private String state;
+
+	private String city;
+
+	Student(String name, String state, String city) {
+		this.name = name;
+		this.state = state;
+		this.city = city;
+
+	}	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return (name + "\n" + state + "\n" + city);
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+}
